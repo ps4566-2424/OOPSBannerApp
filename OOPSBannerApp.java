184bc82b01f53a8916cb9115ba2ab7852d013dfe
Pavni Sharma <ps4566@srmist.edu.in>
2026-02-15 20:22:13 +0530

    feature/UC2: Print OOPS in banner format

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,7 +2,11 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("OOPS");
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println(" *   *   *   *   *   *   *     ");
+        System.out.println(" *   *   *   *   *   *   *     ");
+        System.out.println(" *   *   *   *   *   *   *     ");
+        System.out.println(" *****   *****   *****   ***** ");
 
     }
 }
